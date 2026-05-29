8fb339fc5a9107e785221c43fb49a3d06a07ffb2
nicolasfernandez4 <nicolas.fernandez4@bue.edu.ar>
2026-05-29 06:19:07 -0300

    Taller10

---
+++ Taller10/condicionales.js
@@ -0,0 +1,49 @@
+// let edad=20;
+// let tieneEntrada=true;
+
+// if(edad >=18 && tieneEntrada)
+// {
+// console.console.log("Puede ingresar");
+// }
+// else
+// {
+//     console.console.log("No Puede ingresar");
+// }
+
+
+// let esSocio=true;
+// let tieneEntrada=false;
+
+
+// if(esSocio || tieneEntrada )
+// {
+//    console.console.log("Puede ingresar"); 
+// }
+// else
+// {
+//     console.console.log("No Puede ingresar");
+// }
+
+
+
+// let estaLloviendo=false;
+
+// if(!estaLloviendo)
+// {
+//     console.log("Podemos salir");
+// }
+
+
+let numero=20;
+
+while(numero<=10)
+{
+    console.log("El valor de numero: ",numero);
+    numero++;
+}
+
+do
+{
+    console.log("El valor de numero: ",numero);
+    numero++;
+}while(numero<6)
